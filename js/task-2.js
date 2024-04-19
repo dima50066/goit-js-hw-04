f9c531b9700ddb5a742569e7a96a7a6943306a52
Dmytro <dima50066@gmail.com>
2024-04-19 16:23:55 +0200

    fix mentor

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,13 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
   let totalCalories = 0;
   for (const day of days) {
     totalCalories += day.calories;
   }
 
-  return totalCalories / days.length;
+  return Number(totalCalories / days.length);
 }
 
 console.log(
